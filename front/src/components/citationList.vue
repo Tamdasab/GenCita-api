4e5999680ecc4e18a59bd2c5bc2a145c57e95fa6
sab tam <sabrinatamda5@gmail.com>
2023-12-19 16:10:57 +0100

    refacto_for_gencita_frontend

--- front/src/components/citationList.vue
+++ front/src/components/citationList.vue
@@ -1,17 +1,48 @@
 <template>
+    <div class="search">
+        <div class="bar">
+            <input 
+                type="text" 
+                class="inp"
+                v-model="category"
+                placeholder="search your citation by category">
+        </div>
+        <div class="button">
+            <button @click="searchByCategory">Search</button>
+        </div>
+    </div>
     <div class="citation-list">
         <h3>CITATION LIST</h3>
         <div class="list" id="citation-list">
-            <div v-if="citations">
-                <div v-for="citation in citations" :key="citation.id" :class="`citation-item ${citation.done && 'done'}`">
-                    <div class="citation-content">
-                        <p>
-                            {{ citation.content  }}
-                        </p>
+            <div v-if="!searched || (searchResults && searchResults.length > 0)">
+                <div v-if="!searched">
+                    <div v-for="citation in citations" :key="citation.id" :class="`citation-item ${citation.done && 'done'}`">
+                        <div class="citation-content">
+                            <p>
+                                "{{ citation.text  }}"
+                            </p>
+                            <p class="citation-category">
+                                Category : {{ citation.category }}
+                            </p>
+                        </div>
+                        <div class="actions">
+                            <button class="delete" @click="removeCitation(citation)">Delete</button>
+                        </div>
                     </div>
-
-                    <div class="actions">
-                        <button class="delete" @click="removeCitation(citation)">Delete</button>
+                </div>
+                <div v-else>
+                    <div v-for="citation in searchResults" :key="citation.id" :class="`citation-item ${citation.done && 'done'}`">
+                        <div class="citation-content">
+                          <p>
+                            "{{ citation.text }}"
+                          </p>
+                          <p class="citation-category">
+                            Category : {{ citation.category }}
+                          </p>
+                        </div>
+                        <div class="actions">
+                          <button class="delete" @click="removeCitation(citation)">Delete</button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -26,18 +57,33 @@
     import { ref, onMounted, defineProps, type Ref } from "vue";
 
     interface Citation{
-        content: string;
+        text: string;
         category: string | null;
         createdAt: number;
         done: boolean;
         id: number;
     }
 
+    const category = ref('');
+    const searched = ref(false);
+    const searchResults: Ref<Citation[]> = ref([]);
     const { citations: propCitations} = defineProps(['citations']);
-
     const citations: Ref<Citation[]> = ref(propCitations);
 
-    
+    const searchByCategory = async () => {
+        try{
+            const response = await fetch(`http://localhost:3000/api/citation/category/${category.value}`);
+            if (response.ok) {
+                const data = await response.json();
+                searchResults.value = data;
+                searched.value = true;
+            } else{
+                console.error("no result found");
+            }
+        } catch(error){
+            console.error("error searching citations by category:", error);
+        }
+    }
 
     const fetchCitations = async () => {
         try{
--- front/src/components/logo.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="">
-        <img src="../assets/GenCita.png" alt="Logo" width="200" />
-    </div>
-</template>
